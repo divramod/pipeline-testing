89ae483c785fdc2ddcf450e625d27d5c3102652b
divramod <divramod@gmail.com>
2019-06-28 07:28:32 +0200

    update

--- cicd/pipelines/parallel.groovy
+++ cicd/pipelines/parallel.groovy
@@ -1,33 +1,48 @@
-pipeline {
-    agent none
-    stages {
-        stage('Run Tests') {
+#!/usr/bin/env groovy
+
+def label = "build-jenkins-operator-${UUID.randomUUID().toString()}"
+def home = "/home/jenkins"
+def workspace = "${home}/workspace/build-jenkins-operator"
+def workdir = "${workspace}/src/github.com/jenkinsci/kubernetes-operator/"
+
+podTemplate(label: label,
+        containers: [
+                containerTemplate(name: 'jnlp', image: 'jenkins/jnlp-slave:alpine'),
+                containerTemplate(name: 'go', image: 'golang:1-alpine', command: 'cat', ttyEnabled: true),
+        ],
+        envVars: [
+                envVar(key: 'GOPATH', value: workspace),
+        ],
+        ) {
+
+    node(label) {
+        dir(workdir) {
             parallel {
-                stage('1') {
-                    agent {
-                        label "linux"
-                    }
-                    steps {
-                        sh "echo hello"
-                    }
-                    post {
-                        always {
-                            sh "echo world"
-                        }
-                    }
+              stage('Init') {
+                  timeout(time: 3, unit: 'MINUTES') {
+                      checkout scm
+                  }
+                  container('go') {
+                      sh 'apk --no-cache --update add make git gcc libc-dev'
+                  }
+              }
+
+              stage('Dep') {
+                  container('go') {
+                      sh 'echo "hello"'
+                  }
+              }
+            }
+
+            stage('Test') {
+                container('go') {
+                    sh 'echo "world"'
                 }
-                stage('2') {
-                    agent {
-                        label "linux"
-                    }
-                    steps {
-                        sh "echo hello"
-                    }
-                    post {
-                        always {
-                            sh "echo world"
-                        }
-                    }
+            }
+
+            stage('Build') {
+                container('go') {
+                    sh 'echo "yes"'
                 }
             }
         }

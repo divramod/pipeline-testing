0498cc477eb804b8d9406eab7858d1bbbad7efa1
divramod <divramod@gmail.com>
2019-07-02 11:48:59 +0200

    update

--- cicd/pipelines/dind.groovy
+++ cicd/pipelines/dind.groovy
@@ -28,7 +28,7 @@ pipeline {
         container('dind') {
           sh "echo ${GIT_COMMIT_HASH}"
           sh "docker build . -t jenkins/pipeline-testing:${GIT_COMMIT_HASH}"
-          sh "docker tag jenkins/pipeline-testing:${GIT_COMMIT_HASH} docker.calponia-divramod.de/jenkins/pipeline-testing:${GIT_COMMIT_HASH}"
+          sh "docker tag jenkins/pipeline-testing:${GIT_COMMIT_HASH} 'https://docker.calponia-divramod.de/jenkins/pipeline-testing:'${GIT_COMMIT_HASH}"
           sh "docker push docker.calponia-divramod.de/jenkins/pipeline-testing:${GIT_COMMIT_HASH}"
         }
       }

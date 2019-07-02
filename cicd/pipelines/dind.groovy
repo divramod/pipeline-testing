504b421938560567493082462d6ab335eb4fa79a
divramod <divramod@gmail.com>
2019-07-02 12:37:12 +0200

    update

--- cicd/pipelines/dind.groovy
+++ cicd/pipelines/dind.groovy
@@ -33,26 +33,42 @@ pipeline {
       parallel {
 
         stage('service1') {
+          agent {
+            kubernetes {
+              label 'dind-1'
+              customWorkspace 'some/other/path'
+              // defaultContainer 'dind'
+              yamlFile 'cicd/k8s/Container.dind.yaml'
+            }
+          }
           steps {
-            container('dind1') {
+            // container('dind1') {
               sh "echo ${GIT_COMMIT_HASH}"
               dir("service1") {
                 sh "docker build . -t docker.calponia-divramod.de/jenkins/service1:${GIT_COMMIT_HASH}"
                 sh "docker push docker.calponia-divramod.de/jenkins/service1:${GIT_COMMIT_HASH}"
               }
-            }
+            // }
           }
         }
 
         stage('service2') {
+          agent {
+            kubernetes {
+              label 'dind-2'
+              customWorkspace 'some/other/path'
+              // defaultContainer 'dind'
+              yamlFile 'cicd/k8s/Container.dind.yaml'
+            }
+          }
           steps {
-            container('dindme') {
+            // container('dindme') {
               sh "echo ${GIT_COMMIT_HASH}"
               dir("service2") {
                 sh "docker build . -t docker.calponia-divramod.de/jenkins/service2:${GIT_COMMIT_HASH}"
                 sh "docker push docker.calponia-divramod.de/jenkins/service2:${GIT_COMMIT_HASH}"
               }
-            }
+            // }
           }
         }
 

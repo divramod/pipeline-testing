1047cb7d84605c70efeea326d91a562fdafd266b
divramod <divramod@gmail.com>
2019-07-03 10:50:41 +0200

    update

--- cicd/pipelines/dind.groovy
+++ cicd/pipelines/dind.groovy
@@ -61,9 +61,45 @@ pipeline {
           }
           steps {
             sh "echo ${GIT_COMMIT_HASH}"
-            dir("service1") {
+            dir("service2") {
               sh "docker build . -t docker.calponia-divramod.de/jenkins/service2:${GIT_COMMIT_HASH}"
               sh "docker push docker.calponia-divramod.de/jenkins/service2:${GIT_COMMIT_HASH}"
+            }
+          }
+        }
+
+        stage('service3') {
+          agent {
+            kubernetes {
+              label 'runner-dind-3'
+              customWorkspace 'some/other/path'
+              defaultContainer 'dind'
+              yamlFile 'cicd/k8s/Pod.dind.worker-3.yaml'
+            }
+          }
+          steps {
+            sh "echo ${GIT_COMMIT_HASH}"
+            dir("service3") {
+              sh "docker build . -t docker.calponia-divramod.de/jenkins/service3:${GIT_COMMIT_HASH}"
+              sh "docker push docker.calponia-divramod.de/jenkins/service3:${GIT_COMMIT_HASH}"
+            }
+          }
+        }
+
+        stage('service4') {
+          agent {
+            kubernetes {
+              label 'runner-dind-4'
+              customWorkspace 'some/other/path'
+              defaultContainer 'dind'
+              yamlFile 'cicd/k8s/Pod.dind.worker-4.yaml'
+            }
+          }
+          steps {
+            sh "echo ${GIT_COMMIT_HASH}"
+            dir("service4") {
+              sh "docker build . -t docker.calponia-divramod.de/jenkins/service4:${GIT_COMMIT_HASH}"
+              sh "docker push docker.calponia-divramod.de/jenkins/service4:${GIT_COMMIT_HASH}"
             }
           }
         }

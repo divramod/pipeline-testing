82e8b6af0d24595760ffb986a9912e6d902c336f
divramod <divramod@gmail.com>
2019-07-17 21:07:53 +0200

    feat/cicd: run

---
+++ cicd/jobs/code-review.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+pipelineJob('pipeline-testing-code-review') {
+    displayName('pipeline-testing: code-review')
+
+    logRotator {
+        numToKeep(10)
+        daysToKeep(30)
+    }
+
+    configure { project ->
+        project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.DurabilityHintJobProperty' {
+            hint('PERFORMANCE_OPTIMIZED')
+        }
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('git@github.com:divramod/pipeline-testing.git')
+                        credentials('ssh-key-jenkins-github-pipeline-testing')
+                    }
+                    branches('*/master')
+                }
+            }
+            scriptPath('cicd/pipelines/code-review.groovy')
+        }
+    }
+}
---
+++ cicd/pipelines/code-review.groovy
@@ -0,0 +1,27 @@
+def PATH_BASE = '/home/jenkins/cicd'
+pipeline {
+  agent none
+  environment {
+    PATH = "$PATH_BASE/.bin:/home/jenkins/cicd/scripts:/home/jenkins/cicd/scripts/utils:/home/jenkins/cicd/vendors/argsh/bin:/home/jenkins/cicd/vendors/bats-core/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin"
+  }
+  stages {
+
+    stage('code review') {
+      agent {
+        kubernetes {
+          label 'runner-pipeline-testing-danger'
+          defaultContainer 'danger'
+          customWorkspace '/home/jenkins/cicd'
+          yamlFile 'cicd/k8s/Pod.danger.yaml'
+        }
+      }
+      steps {
+
+        // DEBUG: print env
+        sh 'env'
+
+      }
+    }
+
+  }
+}

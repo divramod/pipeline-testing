3a175c586e3cf4391d7ec39f1c40613f67fe15be
divramod <divramod@gmail.com>
2019-07-17 21:28:36 +0200

    feat/cicd: run

--- cicd/jobs/danger.groovy
+++ cicd/jobs/danger.groovy
@@ -22,7 +22,7 @@ pipelineJob('pipeline-testing-danger') {
                         url('git@github.com:divramod/pipeline-testing.git')
                         credentials('ssh-key-jenkins-github-pipeline-testing')
                     }
-                    branches('*/master')
+                    branches('*/divramod/feat/cicd')
                 }
             }
             scriptPath('cicd/pipelines/danger.groovy')

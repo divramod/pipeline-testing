6a3b338b64e3bb3a65437d486ee03fe98732a594
divramod <divramod@gmail.com>
2019-07-18 22:26:27 +0200

    feat/cicd: run

--- cicd/pipelines/danger.groovy
+++ cicd/pipelines/danger.groovy
@@ -1,6 +1,6 @@
 def PATH_BASE = '/home/jenkins/cicd'
 pipeline {
-  agent none
+  // agent none
   environment {
     PATH = "$PATH_BASE/.bin:/home/jenkins/cicd/scripts:/home/jenkins/cicd/scripts/utils:/home/jenkins/cicd/vendors/argsh/bin:/home/jenkins/cicd/vendors/bats-core/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin"
   }

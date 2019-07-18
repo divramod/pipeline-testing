7e0e158e9fa3e3a00af0ba908f140f5da89855b9
divramod <divramod@gmail.com>
2019-07-18 21:14:31 +0200

    feat/cicd: run

--- cicd/jobs/danger.groovy
+++ cicd/jobs/danger.groovy
@@ -1,31 +1,28 @@
 #!/usr/bin/env groovy
-
 pipelineJob('pipeline-testing-danger') {
-    displayName('pipeline-testing: danger')
-
-    logRotator {
-        numToKeep(10)
-        daysToKeep(30)
+  displayName('pipeline-testing: danger')
+  description('This is the description')
+  logRotator {
+    numToKeep(10)
+    daysToKeep(30)
+  }
+  configure { project ->
+    project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.DurabilityHintJobProperty' {
+      hint('PERFORMANCE_OPTIMIZED')
     }
-
-    configure { project ->
-        project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.DurabilityHintJobProperty' {
-            hint('PERFORMANCE_OPTIMIZED')
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('git@github.com:divramod/pipeline-testing.git')
+            credentials('ssh-key-jenkins-github-pipeline-testing')
+          }
+          branches('master')
         }
+      }
+      scriptPath('cicd/pipelines/danger.groovy')
     }
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('git@github.com:divramod/pipeline-testing.git')
-                        credentials('ssh-key-jenkins-github-pipeline-testing')
-                    }
-                    branches('')
-                }
-            }
-            scriptPath('cicd/pipelines/danger.groovy')
-        }
-    }
+  }
 }

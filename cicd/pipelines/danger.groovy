fc3466f64fe148e4d0e0d5b14127f8eabfb73d1e
divramod <divramod@gmail.com>
2019-07-18 23:05:08 +0200

    feat/cicd: run

--- cicd/pipelines/danger.groovy
+++ cicd/pipelines/danger.groovy
@@ -1,6 +1,7 @@
 def PATH_BASE = '/home/jenkins/cicd'
 
 pipeline {
+
   agent {
     kubernetes {
       label 'runner-pipeline-testing-danger'
@@ -9,14 +10,20 @@ pipeline {
       yamlFile 'cicd/k8s/Pod.danger.yaml'
     }
   }
+
   options {
     skipDefaultCheckout()
   }
-  parameters { text(name: 'MY_TEST', defaultValue: 'no', description: 'Bla blup') }
+
+  parameters {
+    text(name: 'MY_TEST', defaultValue: 'no', description: 'Bla blup')
+  }
+
   environment {
     PATH = "$PATH_BASE/.bin:/home/jenkins/cicd/scripts:/home/jenkins/cicd/scripts/utils:/home/jenkins/cicd/vendors/argsh/bin:/home/jenkins/cicd/vendors/bats-core/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-1.8-openjdk/jre/bin:/usr/lib/jvm/java-1.8-openjdk/bin"
     GIT_BRANCH = "divramod/feat/cicd-test"
   }
+
   stages {
 
     stage('Checkout') {
